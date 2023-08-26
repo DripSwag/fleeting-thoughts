15662dd106c3b685a22606da08f5e432e7f87feb
DripSwag <mimirani33@gmail.com>
2023-08-26 10:01:31 +0100

    Added title to thoughts

--- frontend/app/homepage/Thought.tsx
+++ frontend/app/homepage/Thought.tsx
@@ -1,20 +1,8 @@
-import { useRouter } from '@/node_modules/next/navigation'
-
 interface Params {
   text: string
   id: number
 }
 
 export default function Thoght({ text, id }: Params) {
-  const router = useRouter()
-
-  function handleClick() {
-    router.push('/homepage/thought/' + id.toString())
-  }
-
-  return (
-    <div onClick={handleClick}>
-      <h1>{text}</h1>
-    </div>
-  )
+  return <a href={'/homepage/thought/' + id.toString()}>{text}</a>
 }
---
+++ frontend/app/homepage/thought/[id]/Title.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { FormEvent, useRef } from 'react'
+
+async function updateTitle(title: string | null, id: number) {
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_API_ORIGIN + '/thought',
+    {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title, id }),
+    },
+  )
+
+  return response
+}
+
+interface Params {
+  title: string | null
+  thoughtId: string
+}
+
+export default function Title({ title, thoughtId }: Params) {
+  const headingRef = useRef<HTMLHeadingElement>(null)
+
+  function handleKeyDown(event: React.KeyboardEvent<HTMLHeadingElement>) {
+    if (event.key == 'Enter') {
+      event.preventDefault()
+      headingRef.current?.blur()
+    }
+  }
+
+  async function handleBlur(event: FormEvent<HTMLHeadingElement>) {
+    const response = await updateTitle(
+      event.currentTarget.textContent,
+      parseInt(thoughtId),
+    )
+  }
+
+  return (
+    <h1
+      contentEditable
+      ref={headingRef}
+      suppressContentEditableWarning={true}
+      onBlur={handleBlur}
+      onKeyDown={handleKeyDown}
+      className='min-w-[2em] border-b-2 text-4xl font-bold outline-none'
+    >
+      {title}
+    </h1>
+  )
+}
