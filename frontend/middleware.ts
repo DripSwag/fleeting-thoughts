f0e936b6ad506969967afe211e50476b50c27e83
DripSwag <mimirani33@gmail.com>
2023-09-09 17:45:24 +0100

    Fixed regex for getting thoughtId

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from './node_modules/next/server'
 
 export default async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith('/homepage/thought')) {
-    const thoughtId = request.nextUrl.pathname.match(/\d/g)?.at(0) || ''
+    const thoughtId = request.nextUrl.pathname.match(/\d+/g)?.at(0) || ''
     const response = await fetch(process.env.API_ORIGIN + '/verify/thought', {
       headers: {
         ssid: request.cookies.get('ssid')?.value,
